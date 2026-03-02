a9a362790f3a72c1aa15fc24d9c6eeec690467e7
inwoo <ninw0205@gmail.com>
2026-03-02 15:48:58 +0900

    chore: Init project

---
+++ src/main/java/com/momok/MomokApplication.java
@@ -0,0 +1,13 @@
+package com.momok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momok/MomokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
