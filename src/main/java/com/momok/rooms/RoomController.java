aad01280c72f70db45f0753b977e789e2454a692
inwoo <ninw0205@gmail.com>
2026-03-05 16:54:23 +0900

    docs: swagger 문서화 코드 작성

--- src/main/java/com/momok/rooms/RoomController.java
+++ src/main/java/com/momok/rooms/RoomController.java
@@ -10,15 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.momok.rooms.Dto.VoteRoomRequestDto;
 import com.momok.rooms.Dto.VoteRoomResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/rooms")
+@Tag(name = "VoteRoom", description = "투표 방 관련 API")
 public class RoomController {
 	private final RoomService roomService;
 
 	@PostMapping
+	@Operation(summary = "방 생성", description = "새로운 음식점 투표 방을 생성합니다.")
+	@ApiResponse(responseCode = "201", description = "방이 성공적으로 생성되었습니다.")
 	public ResponseEntity<VoteRoomResponseDto> createVoteRoom(@RequestBody VoteRoomRequestDto voteRoomRequestDto) {
 		if (voteRoomRequestDto == null || voteRoomRequestDto.getLocation() == null) {
 			return ResponseEntity.badRequest().build();
